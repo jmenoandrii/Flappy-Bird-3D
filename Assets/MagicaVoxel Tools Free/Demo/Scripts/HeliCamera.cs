0909240f061608ad58f1e0db7201d89dda766e6b
Andriy <andriyhood@gmail.com>
2021-04-15 01:37:08 +0300

    v0.1
    Added model bird for player;

---
+++ Assets/MagicaVoxel Tools Free/Demo/Scripts/HeliCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HeliCamera : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        this.transform.position = target.transform.position + offset;
+        this.transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/MagicaVoxel Tools Free/Demo/Scripts/HeliRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HeliRotate : MonoBehaviour
+{
+    public Vector3 axis;
+    public float speed = 1;
+
+    private void Update()
+    {
+        this.transform.Rotate(axis, speed * Time.deltaTime);
+    }
+}
